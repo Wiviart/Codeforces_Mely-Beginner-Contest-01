e200c286bfbf3d998e40d81a6ded0f25043eadd8
Wiviart <dungphamhuu95@gmail.com>
2024-03-17 21:24:58 +0700

    First init

---
+++ 1. String Array/Program.cs
@@ -0,0 +1,83 @@
+﻿class Program
+{
+    static string GetValidString(string name)
+    {
+        string strg = "";
+        while (strg.Length < 1 || strg.Length > Math.Pow(10, 5))
+        {
+            // Console.WriteLine($"Enter string {name}: ");
+            strg = Console.ReadLine();
+        }
+
+        return strg;
+    }
+
+    public static bool IsSubsequence(string a, string b)
+    {
+        if (a == b) return false;
+
+        char[] aArray = a.ToCharArray();
+        char[] bArray = b.ToCharArray();
+        int indexA = 0;
+        int indexB = 0;
+
+        while (indexB < bArray.Length)
+        {
+            if (indexA >= aArray.Length)
+            {
+                return true;
+            }
+
+            if (aArray[indexA] == bArray[indexB])
+            {
+                indexA++;
+                indexB++;
+            }
+            else
+            {
+                indexB++;
+            }
+
+            if (indexA >= aArray.Length && indexB >= bArray.Length)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    static void Main()
+    {
+        string a = GetValidString("A");
+        string b = GetValidString("B");
+
+        if (IsSubsequence(a, b))
+            Console.WriteLine("YES");
+        else
+            Console.WriteLine("NO");
+    }
+}
+
+class Tester
+{
+    public static void Test()
+    {
+        string[] c = { "abc", "abcd", "abdssc", "acbd", "aassdb", "oojojac", "oookaijbkkc" };
+
+        string a = "abc";
+        foreach (var x in c)
+        {
+            string b = x;
+
+            if (Program.IsSubsequence(a, b))
+            {
+                Console.WriteLine($"YES = {a} <= {b}");
+            }
+            else
+            {
+                Console.WriteLine($"NO  = {a} <= {b}");
+            }
+        }
+    }
+}
---
+++ 2. Fibonacci/Program.cs
@@ -0,0 +1,95 @@
+﻿using System.Diagnostics;
+using System.Text;
+
+class Program
+{
+    static uint Fibonacci(uint a, uint b, uint n)
+    {
+        if (n == 0) return a;
+        if (n == 1) return b;
+
+        uint fibMinus2 = a,
+            fibMinus1 = b,
+            fib = 0;
+
+        for (int i = 2; i <= n; i++)
+        {
+            fib = fibMinus1 ^ fibMinus2;
+            fibMinus2 = fibMinus1;
+            fibMinus1 = fib;
+        }
+
+        return fib;
+    }
+
+    static string FibonacciCalculator(string testcase)
+    {
+        string[] input = testcase.Split(' ');
+        uint a = uint.Parse(input[0]);
+        uint b = uint.Parse(input[1]);
+        uint n = uint.Parse(input[2]);
+
+        var r = Fibonacci(a, b, n);
+
+        return r.ToString();
+    }
+
+    static void Main()
+    {
+        ushort number = ushort.Parse(Console.ReadLine());
+
+        string[] testcase = new string[number];
+
+        for (ushort i = 0; i < number; i++)
+        {
+            testcase[i] = Console.ReadLine();
+        }
+
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.Start();
+
+        long memoryUsedBefore = GC.GetTotalMemory(true);
+
+        for (int i = 0; i < testcase.Length; i++)
+        {
+            var result = FibonacciCalculator(testcase[i]);
+            Console.WriteLine(result);
+        }
+        // var result = Test();
+
+
+        stopwatch.Stop();
+        Console.WriteLine($"Elapsed time: {stopwatch.ElapsedMilliseconds / 1000} seconds");
+
+        long memoryUsed = GC.GetTotalMemory(true);
+        Console.WriteLine($"Memory used: {(memoryUsed - memoryUsedBefore) / 1000} KB");
+    }
+
+    static bool IsValid(int number, int min, int pow)
+    {
+        if (number < min || number > Math.Pow(10, pow))
+        {
+            return false;
+        }
+        return true;
+    }
+
+    static string Test(int i)
+    {
+        string[] testcase =
+        {
+            "3 5 2",
+            "7 3 0",
+            "191 325 123548",
+            "5 12 98",
+            "8765 1232 12323",
+            "54 12 9480",
+            "876 134 5554"
+        };
+
+        return FibonacciCalculator(testcase[i]);
+    }
+
+
+}
+
